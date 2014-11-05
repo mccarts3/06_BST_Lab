0a36fe1b6e6cf8e98f6c0e784e0732c96e88af8e
mccarts3 <scott2@scotts-MacBook-Pro-5.local>
2014-11-04 19:35:02 -0600

    Put BST.ipp in BST.h, finished basic methods

--- BST.h
+++ BST.h
@@ -47,7 +47,7 @@ private:
   Node<Key,T>* root;
 
   virtual unsigned long size(Node<Key,T>* r);
-  //These are the recursive versions of each of your methods.
+  // These are the recursive versions of each of your methods.
   // You should return the address of the new root node, whether
   // or not the root node changes.
   virtual Node<Key,T>* add(Key k, T x, Node<Key,T>* r);
@@ -66,7 +66,150 @@ private:
   //Find the next/prev node, and return its address
   virtual Node<Key,T>* next(Key k, Node<Key,T>* r);
   virtual Node<Key,T>* prev(Key k, Node<Key,T>* r);
-
+    
+  //CREATED METHOD: Used to delete all nodes, called in the destructor
+  virtual Node<Key, T>* removeAll(Node<Key, T>* r);
 };
 
-#include "BST.ipp"
+/**********************
+ * Previously BST.ipp *
+ *********************/
+
+#define NULL 0
+#include <string>
+
+template <class Key, class T>
+BST<Key,T>::BST(){
+    root = NULL;
+}
+
+template <class Key, class T>
+BST<Key,T>::~BST(){
+    //use a removeAll() method to delete all the nodes
+    removeAll(root);
+}
+
+//My created method for removing all the nodes in the BST
+template <class Key, class T>
+Node<Key, T>* BST<Key,T>::removeAll(Node<Key, T>* r) {
+    //Base case: No nodes connected to it
+    if(r->left == NULL && r->right == NULL) {
+        delete r;
+    }
+    //Case 1: There is a node to the left
+    else if(r->left != NULL) {
+        return removeAll(r->left);
+    }
+    //Case 2: Must be a node to the right
+    else {
+        return removeAll(r->right);
+    }
+}
+
+//Return the number of items currently in the SSet
+template <class Key, class T>
+unsigned long BST<Key,T>::size(){
+    //Return the private size() method using the top node
+    //as the parameter
+    return size(root);
+}
+
+template <class Key, class T>
+unsigned long BST<Key,T>::size(Node<Key,T>* r){
+    if(r == NULL) {
+        return 0;
+    }
+    else {
+        return 1 + size(r->left) + size(r->right);
+    }
+}
+
+//Add a new item, x, with Key k.
+// If an item with Key k already exists, overwrite it
+template <class Key, class T>
+void BST<Key,T>::add(Key k, T x){
+    //TODO
+}
+
+//Remove the item with Key k. If there is no such item, do nothing.
+template <class Key, class T>
+void BST<Key,T>::remove(Key k){
+    //TODO
+}
+
+//Return the item with Key k.
+// If there is no such item, throw an exception.
+template <class Key, class T>
+T BST<Key,T>::find(Key k){
+    //TODO
+    T fakeT;
+    return fakeT;
+}
+//Return true if there is an item with Key k in the table. If not,
+// return false
+template <class Key, class T>
+bool BST<Key,T>::keyExists(Key k){
+    //TODO
+    return false;
+}
+
+//If there is a key in the set that is > k,
+// return the first such key. If not, return k
+template <class Key, class T>
+Key BST<Key,T>::next(Key k){
+    //TODO
+    Key fakeKey;
+    return fakeKey;
+}
+
+template <class Key, class T>
+Node<Key,T>* BST<Key,T>::next(Key k, Node<Key,T>* r){
+    //TODO
+    return NULL;
+}
+
+//If there is a key in the set that is < k,
+// return the first such key. If not, return k
+template <class Key, class T>
+Key BST<Key,T>::prev(Key k){
+    //TODO
+    return NULL;
+}
+
+template <class Key, class T>
+Node<Key,T>* BST<Key,T>::prev(Key k, Node<Key,T>* r){
+    //TODO
+    return NULL;
+}
+
+
+template <class Key, class T>
+Node<Key,T>* BST<Key,T>::add(Key k, T x, Node<Key,T>* r){
+    //TODO
+    return NULL;
+}
+
+template <class Key, class T>
+Node<Key,T>* BST<Key,T>::remove(Key k, Node<Key,T>* r){
+    //TODO
+    return NULL;
+}
+
+template <class Key, class T>
+Node<Key,T>* BST<Key,T>::find(Key k, Node<Key,T>* r){
+    //TODO
+    return NULL;
+}
+
+template <class Key, class T>
+Node<Key,T>* BST<Key,T>::max(Node<Key,T>* r){
+    //TODO
+    return NULL;
+}
+
+template <class Key, class T>
+Node<Key,T>* BST<Key,T>::min(Node<Key,T>* r){
+    //TODO
+    return NULL;
+}
+
